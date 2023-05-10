edc60160bbb9a401645a343c0de7ac4ac99cb3c1
Maja Krmar <majakrmar05@gmail.com>
2023-05-10 16:51:18 +0200

    Create draft landing page react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import styles from './App.module.scss';
+
 export default function App() {
   return (
     <>
-      <header>my header</header>
+      <header>
+        <div className={styles.promoSection}>
+          <span>Welcome!</span>
+          <a href="/#">
+            <span> Ready to check out our new features</span>
+            <span>--></span>
+          </a>
+        </div>
+        <nav className={styles.navigationBar}>
+          <div>Logo</div>
+          <ul>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#">Home</a>
+            </li>
+          </ul>
+          <div>
+            <a href="/#">Get started for free</a>
+          </div>
+        </nav>
+      </header>
       <main>main content</main>
       <footer>my footer</footer>
     </>
